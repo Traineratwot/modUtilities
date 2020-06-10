778472a4ffcb9df2eef13446696989012ec7ba57
Kirill <41589091+Traineratwot@users.noreply.github.com>
2020-06-10 16:16:12 +0300

    1

--- core/components/modutilities/elements/plugins/modutilities.php
+++ core/components/modutilities/elements/plugins/modutilities.php
@@ -1,15 +1,15 @@
 <?php
 	if (!class_exists('modUtilities')) {
 		include_once MODX_CORE_PATH . 'components/modutilities/classes/modUtilities.class.php';
-	}else{
-		$modx->log(MODX::LOG_LEVEL_ERROR,'can`t load class "modUtilities" already declared');
+	} else {
+		$modx->log(MODX::LOG_LEVEL_ERROR, 'can`t load class "modUtilities" already declared');
 	}
 	switch ($modx->event->name) {
 		case 'OnMODXInit':
 			if (class_exists('modutilities') and !isset($modx->util)) {
 				$modx->util = new modUtilities($modx);
-			}else{
-				$modx->log(MODX::LOG_LEVEL_ERROR,'can`t load class "modutilities" already declared');
+			} else {
+				$modx->log(MODX::LOG_LEVEL_ERROR, 'can`t load class "modutilities" already declared');
 			}
 			break;
 		case 'pdoToolsOnFenomInit':
@@ -22,7 +22,7 @@
 					} else {
 						return eval('return $modx->util->' . $method . ';');
 					}
-					$modx->log(MODX::LOG_LEVEL_WARN,'can`t run $modx->util->' . $method . ' ');
+					$modx->log(MODX::LOG_LEVEL_WARN, 'can`t run $modx->util->' . $method . ' ');
 					return FALSE;
 				});
 			}
